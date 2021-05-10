4e3e98f400ae286a109366f009fba958187c0de8
Maderalice <madrealice4@gmail.com>
2021-05-10 16:27:01 -0300

    Exemplo de códigos utilizando while

---
+++ src/br/com/senai/AtividadeTabuada.java
@@ -0,0 +1,35 @@
+package br.com.senai;
+
+import java.util.Scanner;
+
+public class AtividadeTabuada {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * * Crie uam nova classe chamada Tabuada e desenvolva um algoritimo que
+		 * receba uma entrada de dados, Scanner(), pedindo que o usuário
+		 * informe um número inteiro, em seguida apresente a tabuada do valor
+		 * informado pelo usuário;
+		  */
+		 
+		 
+		 Scanner tec = new Scanner(System.in);
+		 System.out.print("Informe um número: ");
+		 
+		 int num = tec.nextInt();
+		 int multiplicador = 1;
+		 
+		 while(multiplicador <= 11) {
+		 System.out.println(num + " X " + multiplicador + " = " + num * multiplicador);
+		 multiplicador++;
+		 }
+		 
+		 
+		
+		
+	
+
+	}
+
+}
